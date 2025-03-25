832084c1e8c76fc73cbccd90b28cec08da4a052e
Grzegorz Cichocki <32648503+cichy1173@users.noreply.github.com>
2025-03-25 12:59:38 +0100

    feat: queue the alarms

--- check_pagerduty_alarms.sh
+++ check_pagerduty_alarms.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 set -xeuo pipefail
 
-# check if key is set
+# Check if API key is set
 if [ -z "$API_KEY" ]; then
-  echo "You had to add API key"
+  echo "You must add an API key"
   exit 1
 fi
 
 # Initialize previous incidents variable
-PREVIOUS_INCIDENTS=""
+declare -A PREVIOUS_INCIDENTS
 
 while true; do
   # Check triggered alarms
   INCIDENTS=$(curl -s -H "Authorization: Token token=$API_KEY" -H "Accept: application/vnd.pagerduty+json;version=2" \
-    "https://api.pagerduty.com/incidents?statuses[]=triggered&user_ids[]=$USER_ID" | jq -r '.incidents')
+    "https://api.pagerduty.com/incidents?statuses[]=triggered&user_ids[]=$USER_ID" | jq -r '.incidents[] | .id')
 
-  if [ "$INCIDENTS" == "[]" ]; then
-    echo "You do not have any triggered alarms"
-  else
-    # Convert incidents to a string for comparison
-    INCIDENT_DETAILS=$(echo "$INCIDENTS" | jq -r '.[] | "\(.id): \(.summary)"')
-    
-    # Check if the current incidents are the same as the previous ones
-    if [ "$INCIDENT_DETAILS" == "$PREVIOUS_INCIDENTS" ]; then
-      echo "No new alarms detected, skipping notification."
-    else
-      echo "You have triggered alarms:"
-      echo "$INCIDENT_DETAILS"
+  for INCIDENT_ID in $INCIDENTS; do
+    # Get incident details
+    INCIDENT_DETAILS=$(curl -s -H "Authorization: Token token=$API_KEY" -H "Accept: application/vnd.pagerduty+json;version=2" \
+      "https://api.pagerduty.com/incidents/$INCIDENT_ID" | jq -r '.incident.summary')
+
+    # Check if the incident is new
+    if [ -z "${PREVIOUS_INCIDENTS[$INCIDENT_ID]}" ]; then
+      echo "New alarm detected: $INCIDENT_ID - $INCIDENT_DETAILS"
       
       # Send alarm info to Home Assistant
-      curl -X POST -H "Content-Type: application/json" -d '{"incident_details": "'"$INCIDENT_DETAILS"'"}' "$HOME_ASSISTANT_URL"
+      curl -X POST -H "Content-Type: application/json" -d '{"incident_id": "'"$INCIDENT_ID"'", "incident_details": "'"$INCIDENT_DETAILS"'"}' "$HOME_ASSISTANT_URL"
       
       # Update the previous incidents variable
-      PREVIOUS_INCIDENTS="$INCIDENT_DETAILS"
+      PREVIOUS_INCIDENTS[$INCIDENT_ID]=$INCIDENT_DETAILS
     fi
-  fi
+  done
+
+  # Remove resolved incidents from previous incidents
+  for INCIDENT_ID in "${!PREVIOUS_INCIDENTS[@]}"; do
+    if ! echo "$INCIDENTS" | grep -q "$INCIDENT_ID"; then
+      unset "PREVIOUS_INCIDENTS[$INCIDENT_ID]"
+    fi
+  done
 
   # Wait for next scan
   sleep $SLEEP_TIME
